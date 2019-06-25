e1cb555d7da851b8f519ed944d64ca068ce84984
Dianfang Zheng <dfgzheng@thoughworks.com>
2019-06-25 15:29:41 +0800

    add unit test and implement  for printReceipt

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -1,6 +1,7 @@
 const {
     loadProductById,
     assembleReceiptItems,
+    print
 } = require('../main');
 
 it('should get Coca Cola when give id is 0001', () => {
@@ -37,3 +38,24 @@ it('should assemble receiptItem when give product array', () => {
         { product: {"id": "0003", "name" : "Pepsi-Cola", "price": 5}, count: 1, totalPrice: 5},
     ]);
 })
+
+it('should print correct text when give receiptItems', () => {
+    const mockRecpeiptItems = [
+        { product: {"id": "0001", "name" : "Coca Cola", "price": 3}, count: 3, totalPrice: 9},
+        { product: {"id": "0002", "name" : "Diet Coke", "price": 4}, count: 2, totalPrice: 8},
+        { product: {"id": "0003", "name" : "Pepsi-Cola", "price": 5}, count: 1, totalPrice: 5},
+    ];
+
+    const result = print(mockRecpeiptItems);
+
+    expect(result).toBe(`
+        Receipts:
+        ----------------------------------------------
+        Coca Cola            3      9
+        Diet Coke            2      8
+        Pepsi-Cola           3      5
+        ----------------------------------------------
+        Price: 22 
+    `);
+
+});
--- main.js
+++ main.js
@@ -7,18 +7,43 @@ function loadProductById(id) {
 function assembleReceiptItems(products) {
     const receitpItems = [];
     products.forEach(product => {
-       const receiptItem = receitpItems.find(item => item.product.id === product.id);
-       if (receiptItem) {
-            receiptItem.count += 1; 
-            receiptItem.totalPrice += product.price; 
-       } else {
-         receitpItems.push({ product, count: 1, totalPrice: product.price });
+       if (product) {
+        const receiptItem = receitpItems.find(item => item.product.id === product.id);
+        if (receiptItem) {
+                receiptItem.count += 1; 
+                receiptItem.totalPrice += product.price; 
+        } else {
+            receitpItems.push({ product, count: 1, totalPrice: product.price });
+        }
        }
     });
     return receitpItems;
 }
 
+
+function print(receitpItems) {
+    let result = "Receipts:\n----------------------------------------------\n"
+    let sumPrice = 0;
+    receitpItems.forEach(item => {
+       result += `${item.product.name}    ${item.count}    ${item.totalPrice}\n` 
+       sumPrice += item.totalPrice;
+    });
+    result += "----------------------------------------------\n"
+    result += `Price: ${sumPrice}`;
+    return result;
+}
+
+function printReceiptReport(ids) {
+    if (ids === null || ids === undefined) {
+        return "[Error]: ids paramters is null or undefined"
+    }
+    const products = ids.map(id => loadProductById(id) );
+    const receiptItems = assembleReceiptItems(products);
+    return print(receiptItems);
+}
+
 module.exports = {
     loadProductById,
     assembleReceiptItems,
+    print,
 }
