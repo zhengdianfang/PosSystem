1be5528e6f341b98a9d63107e32be4a2adc8ffa3
Dianfang Zheng <dfgzheng@thoughworks.com>
2019-06-25 14:57:57 +0800

    add unit test for assembleReceiptItems function

--- __tests__/main.spec.js
+++ __tests__/main.spec.js
@@ -1,5 +1,6 @@
 const {
     loadProductById,
+    assembleReceiptItems,
 } = require('../main');
 
 it('should get Coca Cola when give id is 0001', () => {
@@ -17,3 +18,22 @@ it('should get null when give id is 1111', () => {
 
     expect(product).toEqual(undefined);
 });
+
+it('should assemble receiptItem when give product array', () => {
+    const mockProducts = [
+        {"id": "0001", "name" : "Coca Cola", "price": 3},
+        {"id": "0001", "name" : "Coca Cola", "price": 3},
+        {"id": "0001", "name" : "Coca Cola", "price": 3},
+        {"id": "0002", "name" : "Diet Coke", "price": 4},
+        {"id": "0002", "name" : "Diet Coke", "price": 4},
+        {"id": "0003", "name" : "Pepsi-Cola", "price": 5},
+    ];
+
+    const receiptItems = assembleReceiptItems(mockProducts);
+
+    expect(receiptItems).toEqual([
+        { product: {"id": "0001", "name" : "Coca Cola", "price": 3}, count: 3, totalPrice: 9},
+        { product: {"id": "0002", "name" : "Diet Coke", "price": 4}, count: 2, totalPrice: 8},
+        { product: {"id": "0003", "name" : "Pepsi-Cola", "price": 5}, count: 1, totalPrice: 5},
+    ]);
+})
--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 const store = require('./store');
 
 function loadProductById(id) {
-    console.log(store);
     return store.find(item => item.id === id);
+}
+
+function assembleReceiptItems(products) {
+    const receitpItems = [];
+    products.forEach(product => {
+       const receiptItem = receitpItems.find(item => item.product.id === product.id);
+       if (receiptItem) {
+            receiptItem.count += 1; 
+            receiptItem.totalPrice += product.price; 
+       } else {
+         receitpItems.push({ product, count: 1, totalPrice: product.price });
+       }
+    });
+    return receitpItems;
 }
 
 module.exports = {
     loadProductById,
+    assembleReceiptItems,
 }
